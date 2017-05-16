55a225ef283165180607921efcabd0e1971ef5bf
Manish Prakash <manish@excellencetechnologies.in>
2017-05-16 15:45:10 +0530

    Namespace Changed

--- Controller/Adminhtml/Order/MassDelete.php
+++ Controller/Adminhtml/Order/MassDelete.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace ManishJoy\DeleteOrders\Controller\Adminhtml\Order;
+namespace TheCodingTutor\DeleteOrders\Controller\Adminhtml\Order;
 
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use Magento\Backend\App\Action\Context;
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# ManishJoy_DeleteOrders v1.0.0
+# TheCodingTutor_DeleteOrders v1.0.0
 
 More courses and tutorials about Magento 2 visit:
 
-http://thecodingtutor.com/magento/
+http://thecodingtutor.com/category/magento/
+
+###The extension is free and always will be
+##Charges:
+####Installation: $10
+####Supprt: $20/6 months
